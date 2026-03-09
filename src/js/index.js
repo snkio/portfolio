aad25cde8df58f7157ba6be032bd6abd612de46c
Viacheslav Chesnokov <83201823+snkio@users.noreply.github.com>
2026-03-09 12:46:53 +0500

    Add light theme, fix buttons links

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 import "../scss/style.scss";
+
+const getHTML = document.documentElement;
+const switcher = document.querySelector("#toggletheme");
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light") {
+  switcher.textContent = "🌙";
+  getHTML.setAttribute("data-theme", "light");
+} else {
+  switcher.textContent = "☀️";
+  getHTML.setAttribute("data-theme", "dark");
+}
+
+switcher.addEventListener("click", () => {
+  let currentTheme = switcher.textContent;
+
+  if (currentTheme === "☀️") {
+    switcher.textContent = "🌙";
+    getHTML.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  } else {
+    switcher.textContent = "☀️";
+    getHTML.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
